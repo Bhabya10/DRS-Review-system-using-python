f2a1621cbb76a449dbbab8f825578c83f919b291
Bhabya <64031603+Bhabya10@users.noreply.github.com>
2021-07-06 03:29:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,125 @@
+import tkinter
+import cv2 # pip install opencv-python
+import PIL.Image, PIL.ImageTk # pip install pillow
+from functools import partial
+import threading
+import time
+import imutils
+
+
+
+stream = cv2.VideoCapture("clip.mp4")
+flag = True
+def play(speed):
+    global flag
+    print(f"You clicked on play.  speed is {speed}")
+
+    frame1 = stream.get(cv2.CAP_PROP_POS_FRAMES)
+    stream.set(cv2.CAP_PROP_POS_FRAMES, frame1 + speed)
+
+    grabbed, frame = stream.read()
+    if not grabbed:
+        exit()
+    frame = imutils.resize(frame, width = SET_WIDTH, height = SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image = frame, anchor = tkinter.NW)
+    if flag:
+        canvas.create_text(134,26,fill="brown",font="Times 30 italic bold",text="Decision Pending")
+    flag = not flag
+        
+
+        
+
+
+
+def pending(decision):
+    # 1. Display decision pending image
+    frame = cv2.cvtColor(cv2.imread("pending.png.jpeg"), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width = SET_WIDTH, height = SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image = frame, anchor = tkinter.NW)
+
+    # 2. Wait for 1 second
+    time.sleep(1)
+
+    # 3. Display sponsor image
+    frame = cv2.cvtColor(cv2.imread("sponsor.png.jpeg"), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width = SET_WIDTH, height = SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image = frame, anchor = tkinter.NW)
+
+
+
+    # 4. Wait for 1.5 second
+    time.sleep(1.5)
+
+
+
+    # 5. Display out/not out image
+    if decision == 'out':
+        decisionImg = "out.png.jpeg"
+    else:
+        decisionImg = "not_out.png.jpeg"
+    frame = cv2.cvtColor(cv2.imread(decisionImg), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width = SET_WIDTH, height = SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image = frame, anchor = tkinter.NW)
+
+    # 6. Wait for 1.5 second
+   
+
+def out():
+    thread = threading.Thread(target = pending, args = ("out",))
+    thread.daemon = 1
+    thread.start()
+    print("player is out")
+
+   
+
+def not_out():
+    thread = threading.Thread(target = pending, args = (" not out",))
+    thread.daemon = 1
+    thread.start()
+    print("player is not out")
+
+    
+
+
+# width and height of our main screen
+SET_WIDTH = 710
+SET_HEIGHT = 368
+
+# Tkinter gui starts here
+window = tkinter.Tk()
+window.title("Third Umpire Decision Review Kit")
+cv_img = cv2.cvtColor(cv2.imread("welcome.png.jpeg"),cv2.COLOR_BGR2RGB)
+canvas = tkinter.Canvas(window, width=SET_WIDTH, height=SET_HEIGHT)
+photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
+image_on_canvas = canvas.create_image(0,0, ancho = tkinter.NW, image = photo)
+canvas.pack()
+
+
+# Buttons to control playback
+btn = tkinter.Button(window, text = "<< Previous(fast)", width = 50, command = partial(play, -25))
+btn.pack()
+
+btn = tkinter.Button(window, text = "<< Previous(slow)", width = 50, command = partial(play, -2))
+btn.pack()
+
+btn = tkinter.Button(window, text = " Next(slow) >>", width = 50, command = partial(play, 2))
+btn.pack()
+
+btn = tkinter.Button(window, text = " Next(fast) >>", width = 50, command = partial(play, 25))
+btn.pack()
+
+btn = tkinter.Button(window, text = " Give Out ", width = 50, command = out)
+btn.pack()
+
+btn = tkinter.Button(window, text = " Give Not Out ", width = 50, command = not_out)
+btn.pack()
+
+window.mainloop()
